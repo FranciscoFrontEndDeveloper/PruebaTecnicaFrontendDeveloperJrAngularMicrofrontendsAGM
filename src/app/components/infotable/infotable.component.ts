78cdaba4ad377502e4c2aa7eb5fe6897b4ea51b6
FRANCISCO <pachonetworking@gmail.com>
2025-10-22 15:37:28 -0500

    input filter successfull

--- src/app/components/infotable/infotable.component.ts
+++ src/app/components/infotable/infotable.component.ts
@@ -3,6 +3,8 @@ import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 export interface PeriodicElement {
   name: string;
@@ -104,7 +106,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 ];
 @Component({
   selector: 'app-infotable',
-  imports: [MatTableModule, MatPaginatorModule,MatButtonModule, MatIconModule],
+  imports: [MatTableModule, MatPaginatorModule,MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
   templateUrl: './infotable.component.html',
   styleUrl: './infotable.component.sass'
 })
@@ -127,5 +129,10 @@ export class InfotableComponent implements AfterViewInit {
     /** Toggles the expanded state of an element. */
   toggle(element: PeriodicElement) {
     this.expandedElement = this.isExpanded(element) ? null : element;
+  }
+
+    applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
