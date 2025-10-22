01fb6d89237155a4ba7857d1f903460cc109cd1e
FRANCISCO <pachonetworking@gmail.com>
2025-10-22 13:22:46 -0500

    expansion panel and list sucessfull

--- src/app/components/tableexpant/tableexpant.component.ts
+++ src/app/components/tableexpant/tableexpant.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { Component, signal } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import {MatListModule} from '@angular/material/list';
 @Component({
   selector: 'app-tableexpant',
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, MatExpansionModule, MatListModule],
   templateUrl: './tableexpant.component.html',
   styleUrl: './tableexpant.component.sass'
 })
 export class TableexpantComponent {
-
+  readonly panelOpenState = signal(false);
 }
