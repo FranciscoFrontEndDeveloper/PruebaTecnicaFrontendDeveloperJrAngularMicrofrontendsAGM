a8a77c659b326afc5eaef4fe0ee4e7ba2ef01e66
FRANCISCO <pachonetworking@gmail.com>
2025-10-22 13:57:35 -0500

    component sidenav created

---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+@Component({
+  selector: 'app-sidenav',
+  imports: [MatSidenavModule],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.sass'
+})
+export class SidenavComponent {
+
+}
--- src/app/components/tableexpant/tableexpant.component.ts
+++ src/app/components/tableexpant/tableexpant.component.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
 import {MatListModule} from '@angular/material/list';
+import { SidenavComponent } from "../sidenav/sidenav.component";
 @Component({
   selector: 'app-tableexpant',
-  imports: [MatSidenavModule, MatExpansionModule, MatListModule],
+  imports: [MatSidenavModule, MatExpansionModule, MatListModule, SidenavComponent],
   templateUrl: './tableexpant.component.html',
   styleUrl: './tableexpant.component.sass'
 })
